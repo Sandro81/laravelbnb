94f624eb1da542dd3319b7e4cef8333e4cb9bee6
Sandro81 <sandro.fioravanti81@gmail.com>
2020-01-22 07:28:59 +0000

    Installing and setting up Vue Router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,20 @@
-
 require('./bootstrap');
+
+
+import VueRouter from "vue-router";
+import router from "./routes";
 
 window.Vue = require('vue');
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    'example-component',
+    require('./components/ExampleComponent.vue').default
+);
+
+Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
+    router
 });
+
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,8 +1,16 @@
+import VueRouter from "vue-router";
 import ExampleComponent from "./components/ExampleComponent";
-
+// http://localhost/laravelbnb/public/#/
 const routes = [
     {
         path: "/",
-        component:
-    }
-]
+        component: ExampleComponent,
+        name: "home",
+    },
+];
+
+const router = new VueRouter({
+    routes,
+});
+
+export default router;
