9cea7f02536baa3f2b8704f9b734a4bf77171504
Sandro81 <sandro.fioravanti81@gmail.com>
2020-01-24 21:16:41 +0000

    Routes and model all() method

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('bookables', function (Request $request) {
+    return Bookable::all();
+});
