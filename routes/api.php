7f117530ffb7f43afa29be3743b1c6523812ceae
Sandro81 <sandro.fioravanti81@gmail.com>
2020-01-27 20:13:24 +0000

    Resource controllers

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('bookables', 'Api\BookableController@index');
-Route::get('bookables/{id}', 'Api\BookableController@show');
+Route::get('bookables', 'Api\BookableController');
+
+//Route::get('bookables', 'Api\BookableController@index');
+//Route::get('bookables/{id}', 'Api\BookableController@show');
 
 //Route::get('bookables/{id}', function (Request $request, $id) {
 //    return Bookable::findOrFail($id);
