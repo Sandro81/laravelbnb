d172b23e7bd576169716517ec8a6a018a78da7c4
Sandro81 <sandro.fioravanti81@gmail.com>
2020-01-27 19:46:51 +0000

    Laravel controllers

---
+++ app/Http/Controllers/Api/BookableController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Bookable;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BookableController extends Controller
+{
+    public function index(){
+        return Bookable::all();
+    }
+
+    public function show(){
+        return Bookable::findOrFail($id);
+    }
+}
